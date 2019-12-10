efa79ef40cad71f0f22813bb404fbeef371bada2
Nataliia Hachok <norbinatali@gmail.com>
2019-12-10 19:44:14 +0200

    Update navs.js

--- src/menu/navbar/navs.js
+++ src/menu/navbar/navs.js
@@ -2,10 +2,18 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import Nav from './nav';
+import i18n from "../translation/i18n";
+import LogoV2 from "../style/LogoMakr_6pZrzB.png";
+import Toolbar from "@material-ui/core/Toolbar";
+import FlagUA from "../style/LogoMakr_1Xl0t4.png";
+import FlagUS from "../style/LogoMakr_4V1dPm.png";
 
 
 const DEFAULT_NAV_WIDTH = 100;
+const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
 
+};
 const Navs = ({ items, offset, duration, delay, coverWidth, navWidth, linkClass, activeLinkClass }) => {
     navWidth = navWidth ? navWidth : DEFAULT_NAV_WIDTH;
     coverWidth = coverWidth ? coverWidth : items.length * navWidth;
@@ -19,7 +27,7 @@ const Navs = ({ items, offset, duration, delay, coverWidth, navWidth, linkClass,
     };
     const navsStyle = {
         margin: 0,
-        left: coverWidth /2  - 50,
+        left: coverWidth /2  - 100,
         paddingLeft: 0,
         position: "relative",
         height: "100%",
@@ -32,7 +40,9 @@ const Navs = ({ items, offset, duration, delay, coverWidth, navWidth, linkClass,
 
     return (
         <div style={coverStyle}>
+
             <ul style={navsStyle} ref={navsNode}>
+                <img src={LogoV2}/>
                 {items.map((item, i) => (
                     <Nav
                         key={i}
@@ -46,7 +56,10 @@ const Navs = ({ items, offset, duration, delay, coverWidth, navWidth, linkClass,
                         linkClass={linkClass}
                         activeLinkClass={activeLinkClass} />
                 ))}
+                <button style={{height:"40%", backgroundColor:"rgba(0,1,14,0.74)",border:"none",outline:"none"}} onClick={() => changeLanguage('ua')}><img src={FlagUA}/></button>
+                <button style={{height:"40%",backgroundColor:"rgba(0,1,14,0.86)", border:"none",outline:"none"}} onClick={() => changeLanguage('en')}><img src={FlagUS}/></button>
             </ul>
+
         </div>
     );
 };
@@ -58,6 +71,6 @@ Navs.propTypes = {
     delay: PropTypes.number,
     coverWidth: PropTypes.number,
     navWidth: PropTypes.number
-}
+};
 
 export default Navs;
