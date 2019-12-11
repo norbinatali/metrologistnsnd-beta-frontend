f28b22e7d0e637661ee5daeda0cfad01eb596e8d
Nataliia Hachok <norbinatali@gmail.com>
2019-12-11 20:45:57 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
                     <Switch>
                         <Router history={history}>
-                    <Route exact path="/" component={Login} />
+                    <Route exact path="/" component={MenuTabPanel} />
                             <Route exact path="/reset-password" component={ForgetPassword} />
                             <Route exact path={"/reset-password/"+resetToken} component={ConfirmResetPassword}/>
                     <Route exact path="/signup" component={SignUp} />
