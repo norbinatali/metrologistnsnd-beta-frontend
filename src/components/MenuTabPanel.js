98d7b8e8a27dcd1ae25c770bebe86d70463b2b59
Nataliia Hachok <norbinatali@gmail.com>
2019-12-12 15:57:21 +0200

    Update MenuTabPanel.js

--- src/components/MenuTabPanel.js
+++ src/components/MenuTabPanel.js
@@ -142,8 +142,8 @@ function MenuTabPanel({t}) {
 </Grid>
   <Grid item spacing={6}>
                     <div style={{marginLeft:"auto"}}>
-                        <button style={{height:"10%", backgroundColor:"rgba(0,1,14,0.74)",border:"none",outline:"none"}} onClick={() => changeLanguage('ua')}><img src={FlagUA}/></button>
-                        <button style={{height:"10%", backgroundColor:"rgba(0,1,14,0.74)",border:"none",outline:"none"}} onClick={() => changeLanguage('en')}><img src={FlagUS}/></button>
+                        <button style={{height:"10%",width:"25%", backgroundColor:"rgba(0,1,14,0.74)",border:"none",outline:"none"}} onClick={() => changeLanguage('ua')}><img src={FlagUA}/></button>
+                        <button style={{height:"10%", width:"25%", backgroundColor:"rgba(0,1,14,0.74)",border:"none",outline:"none"}} onClick={() => changeLanguage('en')}><img src={FlagUS}/></button>
                     </div>
 </Grid>
 </Grid>
