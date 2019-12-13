ba528daaf89aeb75ebb061823f2efa186a39fafb
Nataliia Hachok <norbinatali@gmail.com>
2019-12-13 10:31:03 +0200

    Update SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -36,70 +36,78 @@ class SignUp extends Component{
         const userId = localStorage.getItem(GC_USER_ID);
         const { t } = this.props;
         return ( 
- <div style={{flexGrow: 1, display: 'flex', alignItems: 'center',justifyContent: 'center'}}>
+ <div style={{height: '100%'}} >
+                <MuiThemeProvider>
+                <Grid container style={{ height: '100%'}} >
 
-                <Grid container spacing={6}  >
-                    <div >
+                    <Grid item lg={5}>
+                    <div style={{marginTop:"0px"}} >
                         <IconButton onClick={this.handleBack}>
                             <ArrowBackIcon style={{color:"white"}} />
                         </IconButton>
                     </div>
-                <FormControl style={{marginLeft:"20%"}}>
-            <h3 style={{color:"#fff"}} >{t("Registration")}</h3><br/>
-                <MuiThemeProvider>
-       <label style={{color:"#fff"}} htmlFor="email">{t("Email")} </label>
-                    <TextField
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        style={{backgroundColor:"#fff"}}
-                        value={this.state.email}
-                        onChange={e => {
-                            this.setState({ email: e.target.value })
-                        }}
-                        required
-                    />
-       < label style={{color:"#fff"}} htmlFor="password">{t("Password")} </label>
-                    <TextField
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        style={{backgroundColor:"#fff"}}
-                        value={this.state.password}
-                        onChange={e => {
-                            this.setState({ password: e.target.value })
-                        }} required
-                    />
-       <label style={{color:"#fff"}} htmlFor="name">{t("Name")} </label>
-                    <TextField
-                        type="text"
-                        fullWidth
-                        variant="outlined"
-                        style={{backgroundColor:"#fff"}}
-                        value={this.state.name}
-                        onChange={e => {
-                            this.setState({ name: e.target.value })
-                        }}
-                    />
-                    <label style={{color:"#fff"}} htmlFor="companyName">{t("Company Name")} </label>
-                    <TextField
-                        type="text"
-                        value={this.state.companyName}
-                        fullWidth
-                        variant="outlined"
-                        style={{backgroundColor:"#fff"}}
-                        onChange={e => {
-                            this.setState({ companyName: e.target.value })
-                        }}
-                        required
-                    /><br/>
-                    <Mutation mutation={SIGNUP_MUTATION}  variables={{ email, password, name,companyName } } onCompleted={() => this._confirm()}>
-                        {signup => (
-                            <RaisedButton onClick={signup}>{t("Submit")} </RaisedButton>)}
-                    </Mutation>
+                    </Grid>
+                    <Grid
+                        item
+                        lg={7}
+                        xs={12}
+                    >
+                    <FormControl style={{flexGrow: 1, display: 'flex', alignItems: 'center'}}>
+                        <h3 style={{color:"#fff"}} >{t("Registration")}</h3><br/>
 
+                            <label style={{color:"#fff"}} htmlFor="email">{t("Email")} </label>
+                            <TextField
+                                type="text"
+                                fullWidth
+                                variant="outlined"
+                                style={{backgroundColor:"#fff"}}
+                                value={this.state.email}
+                                onChange={e => {
+                                    this.setState({ email: e.target.value })
+                                }}
+                                required
+                            />
+                            < label style={{color:"#fff"}} htmlFor="password">{t("Password")} </label>
+                            <TextField
+                                type="text"
+                                fullWidth
+                                variant="outlined"
+                                style={{backgroundColor:"#fff"}}
+                                value={this.state.password}
+                                onChange={e => {
+                                    this.setState({ password: e.target.value })
+                                }} required
+                            />
+                            <label style={{color:"#fff"}} htmlFor="name">{t("Name")} </label>
+                            <TextField
+                                type="text"
+                                fullWidth
+                                variant="outlined"
+                                style={{backgroundColor:"#fff"}}
+                                value={this.state.name}
+                                onChange={e => {
+                                    this.setState({ name: e.target.value })
+                                }}
+                            />
+                            <label style={{color:"#fff"}} htmlFor="companyName">{t("Company Name")} </label>
+                            <TextField
+                                type="text"
+                                value={this.state.companyName}
+                                fullWidth
+                                variant="outlined"
+                                style={{backgroundColor:"#fff"}}
+                                onChange={e => {
+                                    this.setState({ companyName: e.target.value })
+                                }}
+                                required
+                            /><br/>
+                            <Mutation mutation={SIGNUP_MUTATION}  variables={{ email, password, name } } onCompleted={() => this._confirm()}>
+                                {signup => (
+                                    <RaisedButton onClick={signup}>{t("Submit")} </RaisedButton>)}
+                            </Mutation>
+                    </FormControl>
 
-                </MuiThemeProvider> </FormControl></Grid>
+                    </Grid> </Grid></MuiThemeProvider>
             </div>
         );
     }
