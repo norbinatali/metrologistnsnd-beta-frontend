14e53acde321b3d780dca2f9881c7f02bcbf36f9
Nataliia Hachok <norbinatali@gmail.com>
2019-12-13 13:38:40 +0200

    Update ContactUS.js

--- src/components/ContactUS.js
+++ src/components/ContactUS.js
@@ -36,9 +36,9 @@ const {t}= this.props;
                       <MuiThemeProvider>
          <Grid container spacing={5} >
                                      <Grid item xs={12} md={4} >
-                                        <Typography style={{color:"#fff"}}>  <h4 >{t('If you have any questions or recommendations, please fill the form.')}</h4></Typography>
+                                        <Typography style={{color:"#fff", marginTop:"50%"}}>  <h4 >{t('If you have any questions or recommendations, please fill the form.')}</h4></Typography>
                                     </Grid>
-                                    <Grid item xs={12} md={4} >>
+                                    <Grid item xs={12} md={4} >
                                         <FormControl >
                     <label style={{color:"#fff"}} htmlFor="from">{t('Email')} </label>
                     <TextField  fullWidth
