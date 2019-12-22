94a39b6d3d280bf42302ce35a0964f65f1461e6c
Nataliia Hachok <norbinatali@gmail.com>
2019-12-22 13:23:51 +0200

    Update Standards.js

--- src/components/Standards.js
+++ src/components/Standards.js
@@ -11,21 +11,15 @@ import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
 import { withTranslation} from 'react-i18next';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import IconButton from "@material-ui/core/IconButton";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import Grid from "@material-ui/core/Grid";
 import Paper from '@material-ui/core/Paper';
 import CardContent from "@material-ui/core/CardContent";
-import i18n from "../menu/translations/i18n";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import LinearDeterminate from './LinearDeterminate';
-import FlagUA from "../menu/style/LogoMakr_1Xl0t4.png";
-import FlagUS from "../menu/style/LogoMakr_4V1dPm.png";
-import history from "../history"
-import logo from "../menu/style/LogoMakr_6pZrzB.png"
+import i18n from "../menu/translation/i18n";
 import UserMenu from "./UserMenu";
+import LinearDeterminate from "./LinearDeterminate";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+
 
 const StyledTableCell = withStyles(theme => ({
     head: {
@@ -37,28 +31,22 @@ const StyledTableCell = withStyles(theme => ({
     },
 }))(TableCell);
 
-const StyledTableRow = withStyles(theme => ({
-    root: {
-        '&:nth-of-type(odd)': {
-            backgroundColor: theme.palette.background.default,
-        },
-    },
-}))(TableRow);
+
 const useStyles = makeStyles(theme => ({
     root: {
-        height:"100%",
-        width: '100%',
-         marginRight:"auto",
-        marginLeft:"auto",
-        marginTop: theme.spacing(3),
+        width: '60%',
         overflowX: 'auto',
+        marginRight:"auto",
+        marginLeft:"auto",
+        height:"100%"
     },
     table: {
-      maxWidth:"100%",
+maxWidth:"100%",
         maxHeight:"500px",
+
         color:"#fff"
     },
-     inner:{
+    inner:{
         minWidth: "100%"
     },
     row:{
@@ -73,13 +61,6 @@ const GET_Device = gql`query { allDevice {id name_EN name_UA  module tr{name_TR_
 function Standards({t}){
     const classes = useStyles();
     const [completed, setCompleted] = React.useState(0);
-    const handleBack = () => {
-        history.goBack();
-    };
-     const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
-
-    };
     React.useEffect(() => {
         function progress() {
             setCompleted(oldCompleted => {
@@ -94,29 +75,29 @@ function Standards({t}){
         };
     }, [1]);
     return (
-         <div>
+        <div>
             <UserMenu/>
             <main style={{ flexGrow: 1, height: '100%', overflow: 'auto'}}>
- <Grid container spacing={1}>
-                <Grid item >
-                    <IconButton onClick={handleBack}>
-                            <ArrowBackIcon style={{color:"white"}} />
-                        </IconButton>
-                    </div>
-        </Grid>
-        <Grid item xs={12}>
-              
-                    <Query query={GET_Device} >
-                            {( {loading, error, data} ) =>  {
-                              if (loading) {return <LinearDeterminate/>}
-                                if (error) { return <div>error</div>;}
-                                const devicelist = data.allDevice;
 
-                                if (i18n.language === "ua" )  {
-                                return(
-                                    <Paper>
-                                    <Table className={classes.table} stickyHeader aria-label="sticky table">
-                                        <TableHead>
+        <div  className={classes.root}>
+            <Grid container spacing={1}>
+                <Grid item >
+                    <IconButton >
+                        <ArrowBackIcon style={{color:"white", marginTop:"5%", marginRight:"auto"}} />
+                    </IconButton>
+                </Grid>
+                <Grid item xs={12}>
+            <Query query={GET_Device} >
+                {( {loading, error, data} ) =>  {
+                    if (loading) {return<LinearDeterminate/>}
+                    if (error) { return <div>error</div>;}
+                    const devicelist = data.allDevice;
+
+                    if (i18n.language === "ua" )  {
+                        return(
+                            <Paper className={classes.table} >
+                                    <Table stickyHeader aria-label="sticky table">
+                                        <TableHead >
                                             <TableRow >
                                                 <StyledTableCell align="right">{t('Device')}</StyledTableCell>
                                                 <StyledTableCell align="right">{t('Technical Reglament')}</StyledTableCell>
@@ -125,54 +106,58 @@ function Standards({t}){
                                             </TableRow>
                                         </TableHead>
                                         <TableBody>
-                                             {devicelist.map(device =>(
-                                            <TableRow>
-                                                <TableCell className={classes.row} lign="center">{device.name_UA}</TableCell>
-                                                 <TableCell className={classes.row} lign="center">{device.tr.name_TR_UA}</TableCell>
-                                            <TableCell className={classes.row} lign="center">{device.dtc.name_UA}</TableCell>
-                                             <TableCell className={classes.row} lign="center">{device.module}</TableCell>
-                                            </TableRow>  ))}
-                                            </TableBody>
-                                                                                
-                                             
+                                            {devicelist.map(device =>(
+                                                <TableRow>
+                                                    <TableCell className={classes.row} lign="center">{device.name_UA}</TableCell>
+                                                    <TableCell className={classes.row} lign="center">{device.tr.name_TR_UA}</TableCell>
+                                                    <TableCell className={classes.row} lign="center">{device.dtc.name_UA}</TableCell>
+                                                    <TableCell className={classes.row} lign="center">{device.module}</TableCell>
+                                                </TableRow>  ))}
+                                        </TableBody>
+
+
                                     </Table>
-                                 </Paper>)}
+
+                               </Paper>)}
 
 
 
 
-                                if (i18n.language === "en"){
-                                   return(
-                                        <Paper>
-                                   <Table className={classes.table} stickyHeader aria-label="sticky table">
-                                    <TableHead>
-                                    <TableRow >
-                                    <StyledTableCell align="right">{t('Device')}</StyledTableCell>
-                                    <StyledTableCell align="right">{t('Technical Reglament')}</StyledTableCell>
-                                    <StyledTableCell align="right">{t('Category')}</StyledTableCell>
-                                    <StyledTableCell align="right">{t('Module')}</StyledTableCell>
-                                    </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                      {devicelist.map(device =>(
-                                            <TableRow>
-                                                <TableCell className={classes.row} lign="center">{device.name_EN}</TableCell>
-                                                 <TableCell className={classes.row} lign="center">{device.tr.name_TR_EN}</TableCell>
-                                            <TableCell className={classes.row} lign="center">{device.dtc.name_EN}</TableCell>
-                                             <TableCell className={classes.row} lign="center">{device.module}</TableCell>
-                                            </TableRow>  ))}
-                                            </TableBody>
-                                       </Table> 
-                                    </Paper>)}
-                            else return null}
-                            }
-                        </Query>
-        </Grid>
-        </Grid>
+                    if (i18n.language === "en"){
+                        return(
+                            <Paper style={{width:"100%", maxHeight:"350px"}}>
+                                <CardContent className={classes.content}>
 
-   
+                                    <Table component={Paper} className={classes.table} aria-label="customized table">
+                                        <TableHead aria-label="sticky table">
+                                            <TableRow >
+                                                <StyledTableCell align="right">{t('Device')}</StyledTableCell>
+                                                <StyledTableCell align="right">{t('Technical Reglament')}</StyledTableCell>
+                                                <StyledTableCell align="right">{t('Category')}</StyledTableCell>
+                                                <StyledTableCell align="right">{t('Module')}</StyledTableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {devicelist.map(device =>(
+                                                <TableRow>
+                                                    <TableCell lign="center">{device.name_EN}</TableCell>
+                                                    <TableCell lign="center">{device.tr.name_TR_EN}</TableCell>
+                                                    <TableCell lign="center">{device.dtc.name_EN}</TableCell>
+                                                    <TableCell lign="center">{device.module}</TableCell>
+                                                </TableRow>  ))}
+                                        </TableBody>
+                                    </Table>
+
+                                </CardContent></Paper>)}
+                    else return null}
+                }
+            </Query>
+                </Grid>
+            </Grid>
+        </div>
             </main>
         </div>
+
     );
 
 }
