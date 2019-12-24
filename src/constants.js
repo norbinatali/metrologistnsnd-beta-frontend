1b934cc1ff06573c49dfdfa210599457961ceae2
Nataliia Hachok <norbinatali@gmail.com>
2019-12-24 12:03:13 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,4 @@ export const GC_AUTH_TOKEN = 'graphcool-auth-token';
 export const AUTH_TOKEN = 'auth-token';
 export const RESET_TOKEN = 'uuid';
 export const CONFIRM_EMAIL ='uuid';
-     
+export const CREATE_MY_DEVICE ='create-my-device';     
