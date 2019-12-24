b635e08b0254dbb737a8a1e8407decc392cb4d60
Nataliia Hachok <norbinatali@gmail.com>
2019-12-24 10:41:05 +0200

    Update AddDevice.js

--- src/components/AddDevice.js
+++ src/components/AddDevice.js
@@ -5,6 +5,12 @@ import TextField from "@material-ui/core/TextField";
 import {makeStyles} from "@material-ui/core";
 import RaisedButton from 'material-ui/RaisedButton';
 import Button from '@material-ui/core/Button';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
 const useStyles = makeStyles(theme => ({
     root: {
         width: '100%',
@@ -12,55 +18,149 @@ const useStyles = makeStyles(theme => ({
     },
 
 }));
-function AddDevice ({t}) {
+const useStylesReddit = makeStyles(theme => ({
+    root: {
+        border: '1px solid #e2e2e1',
+        overflow: 'hidden',
+        borderRadius: 4,
+        backgroundColor: '#fcfcfb',
+        transition: theme.transitions.create(['border-color', 'box-shadow']),
+        '&:hover': {
+            backgroundColor: '#fff',
+        },
+        '&$focused': {
+            backgroundColor: '#fff',
+            boxShadow: `${fade(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
+            borderColor: theme.palette.primary.main,
+        },
+    },
+    focused: {},
+}));
+function RedditTextField(props) {
+    const classes = useStylesReddit();
+
+    return <TextField InputProps={{ classes, disableUnderline: true }} {...props} />;
+}
+const CREATE_MYDEVICE =gql `mutation($brand_device:String!, $type_device:String!, $module_device:String!,$notes:String, $verification_device:String, $calibration:Date, $next_calibration:Date){createNewMyDevice(module_device:$module_device, brand_device:$brand_device,type_device: $type_device, notes:$notes, verification_device:$verification_device, calibration:$calibration,next_calibration:$next_calibration){
+    brand_device
+    type_device
+    module_device
+    calibration
+    next_calibration
+}}`;
+function AddDevice ({t,props}) {
     const classes = useStyles();
+  const [brand_device, setStateBrand_device]=useState("");
+    const [type_device, setStateType_device]=useState("");
+    const [module_device, setStateModule_device]=useState("");
+     const [notes, setStateNotes]=useState("");
+     const [verification_device, setStateVerification_device]=useState("");
+    const [calibration, setStateCalibration]=useState(new Date());
+    const [next_calibration, setStateNext_calibration]=useState(new Date());
+    const [addmydevice] = useState(true);
+ const handleBack = () => {
+        history.goBack();
+    };
+    const confirm = async (data, e) => {
 
-
+        const {id, token } = addmydevice;
+        saveUserData(id, token);
+        history.push('/mydevice')
+    };
+    const saveUserData = (id,token) => {
+        localStorage.setItem(GC_USER_ID, id);
+        localStorage.setItem(CREATE_MY_DEVICE, token)
+    };
+    const handleDateCalibration = date => {
+        setStateCalibration(date);
+    };
+    const handleDateNext_Calibration = date => {
+        setStateNext_calibration(date);
+    };
     return(
+        <div>
+        <UserMenu />
         <div className={classes.root}>
-            <FormControl>
-                <label  htmlFor="name">Name</label>
-                <TextField type="text" value=""/>
+            <MuiThemeProvider>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container style={{ height: '100%'}} >
+                    <Grid item >
+                        <div style={{marginTop:"0px"}} >
+                            <IconButton onClick={handleBack}>
+                                <ArrowBackIcon style={{color:"white"}} />
+                            </IconButton>
+                        </div>
+                    </Grid>
+                    <Grid item lg={10} xs={12}>
+                        <Mutation mutation={CREATE_MYDEVICE}  variables={{ brand_device, type_device,module_device, calibration, next_calibration} } onCompleted={() => confirm()}>
+                            {( addmydevice,{loading, error, event}) => {
+                                if (loading) { return (<span>loading</span> )}
+                                if (error) {return (error.message)}
+                                return(
+                        <FormControl style={{flexGrow: 1, display: 'flex', alignItems: 'center', width:"100%"}}>
+
 
                 <label  htmlFor="brand">Brand</label>
-                <TextField
-                    type="text"
-                    value=""
-
-                />
+                <RedditTextField type="text" fullWidth value={brand_device}  onChange={e => {
+                    setStateBrand_device(e.target.value);
+                }}
+                           required/>
 
                 <label  htmlFor="type">Type</label>
-                <TextField
+                <RedditTextField
                     type="text"
-                    value=""
-
-                />
-
-                <label  htmlFor="tr">TR</label>
-                <TextField
-                    type="text"
-                    value=""
-
+                    fullWidth
+                    value={type_device}
+                    onChange={e => {
+                        setStateType_device(e.target.value);
+                    }}
                 />
 
                 <label  htmlFor="module">Module</label>
-                <TextField
+                <RedditTextField
                     type="text"
-                    value=""
-
+                    fullWidth
+                    value={module_device}
+                    onChange={e => {
+                        setStateModule_device(e.target.value);
+                    }}
                 />
-
                 <label  htmlFor="calibration">Calibration</label>
-                <TextField
-                    type="text"
-                    value=""
 
-                />
+                            <KeyboardDatePicker
+                                margin="normal"
+                                id="date-picker-dialog"
+                                label="calibration"
+                                format="MM/dd/yyyy"
+                                value={calibration}
+                                onChange={handleDateCalibration}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change date',
+                                }}
+                            />
+                            <KeyboardDatePicker
+                                margin="normal"
+                                id="date-picker-dialog"
+                                label="next calibration"
+                                format="MM/dd/yyyy"
+                                value={next_calibration}
+                                onChange={handleDateNext_Calibration}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change date',
+                                }}
+                            />
 
-                <Button style={{backgroundColor:"rgba(0,1,47,0.84)", color:"white"}}>{t('Add')}</Button>
-            </FormControl>
+
+                <RaisedButton onClick={addmydevice} style={{backgroundColor:"rgba(0,1,47,0.84)", color:"white"}}>{t('Add')}</RaisedButton>
+            </FormControl>)}}
+                        </Mutation>
+                    </Grid>
+                </Grid>
+                </MuiPickersUtilsProvider>
+            </MuiThemeProvider>
 
         </div>
+</div>
     )
 }
 export default withTranslation()(AddDevice)
