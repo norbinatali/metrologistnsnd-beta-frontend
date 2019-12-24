2f0274558f34b04dbfae628f2b9fde073fb1034e
Nataliia Hachok <norbinatali@gmail.com>
2019-12-24 11:48:38 +0200

    Update AddDevice.js

--- src/components/AddDevice.js
+++ src/components/AddDevice.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import FormControl from '@material-ui/core/FormControl';
 import {withTranslation} from "react-i18next";
 import TextField from "@material-ui/core/TextField";
-import {makeStyles} from "@material-ui/core";
+import PropTypes from 'prop-types';
+import {Grid, IconButton, makeStyles} from "@material-ui/core";
+import history from '../history.js'
 import RaisedButton from 'material-ui/RaisedButton';
 import Button from '@material-ui/core/Button';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import ArrowBackIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import{Mutation} from 'react-apollo';
+import gql from "graphql-tag";
+import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
+import {AUTH_TOKEN, CREATE_MY_DEVICE, GC_USER_ID} from "../constants";
+import {fade} from "@material-ui/core/styles";
+
 import {
     MuiPickersUtilsProvider,
     KeyboardTimePicker,
