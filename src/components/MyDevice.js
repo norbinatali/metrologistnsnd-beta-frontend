0a00b2c21ba7e1937a9ec1ac5d2808656dab041a
Nataliia Hachok <norbinatali@gmail.com>
2020-01-02 14:21:21 +0200

    Update MyDevice.js

--- src/components/MyDevice.js
+++ src/components/MyDevice.js
@@ -88,7 +88,7 @@ function MyDevice({t}){
                 </Grid>
              <Grid item lg={4} md={6} xl={3}  xs={12}  >
            
-                        <MyDeviceForm />
+                       
                 </Grid>
             </Grid>
         </div>
