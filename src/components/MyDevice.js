1a5a34fbe881705fccfad1940b7250586e5aa73c
Nataliia Hachok <norbinatali@gmail.com>
2020-01-02 09:36:26 +0200

    Update MyDevice.js

--- src/components/MyDevice.js
+++ src/components/MyDevice.js
@@ -1,198 +1,142 @@
-import React from "react";
-import {withTranslation} from "react-i18next";
-import {makeStyles} from "@material-ui/core";
-import { withStyles } from '@material-ui/core/styles';
-import ExpandMoreIcon from "@material-ui/core/SvgIcon/SvgIcon";
-import Typography from "@material-ui/core/Typography";
-import MuiExpansionPanel from '@material-ui/core/ExpansionPanel';
-import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import MuiExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Table from "@material-ui/core/Table";
-import TableHead from "@material-ui/core/TableHead";
-import TableCell from "@material-ui/core/TableCell";
-import TableBody from "@material-ui/core/TableBody";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import clsx from 'clsx';
-import Button from '@material-ui/core/Button';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
-import AddDevice from "./AddDevice";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import RefreshIcon from '@material-ui/icons/Refresh';
-import { Link as RouterLink, withRouter } from 'react-router-dom';
-import TableRow from "@material-ui/core/TableRow";
+import React from 'react';
+
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
+import { withTranslation} from 'react-i18next';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import Paper from '@material-ui/core/Paper';
+import CardContent from "@material-ui/core/CardContent";
 import i18n from "../menu/translations/i18n";
-import Link from "@material-ui/core/Link";
 import UserMenu from "./UserMenu";
-import MyDeviceForm from "./MyDeviceForm";
 import LinearDeterminate from "./LinearDeterminate";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import history from '../history';
 import {AUTH_TOKEN, GC_USER_ID} from "../constants";
 import DeleteIcon from "@material-ui/icons/Delete"
-const drawerWidth = 240;
- const userId = localStorage.getItem(GC_USER_ID);
-const authToken = localStorage.getItem(AUTH_TOKEN)
-const useStyles = makeStyles(theme => ({
-    root: {
-           overflow: 'auto',
-        marginRight:"auto",
-        marginLeft:"auto",
-        height:"100%"
-
-    },
-    row: {
-        height: '42px',
-        display: 'flex',
-        alignItems: 'center',
-        marginTop: theme.spacing(1)
-    },
-    spacer: {
-        flexGrow: 1
-    },
-    button: {
-        width:"100 px",
-        height:"10 px",
-        backgroundColor:"white",
-        textColor:"white",
-        marginRight: theme.spacing(3)
-    },
-    input: {
-        display: 'none',
-    },
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        flexBasis: '50%',
-        flexShrink: 0,
-        display:"center",
-    },
-    secondaryHeading: {
-        fontSize: theme.typography.pxToRem(15),
-        color: theme.palette.text.secondary,
-    },
-    margin: {
-        margin: theme.spacing(1),
-    },
-    extendedIcon: {
-        marginRight: theme.spacing(3),
-    },
-    container: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
-        height: 240,
-        width: `calc(100% - ${drawerWidth}px)`,
-    },
-    content: {
-        margin: '40px 16px',
-    },
-  table:{
-        maxWidth:"100%",
-        maxHeight:"500px",
-
-        color:"#fff"
-    }
-
-}));
+const authToken = localStorage.getItem(AUTH_TOKEN);
 
 const StyledTableCell = withStyles(theme => ({
     head: {
         backgroundColor: theme.palette.common.black,
         color: theme.palette.common.white,
-       
     },
     body: {
         fontSize: 12,
-       
     },
 }))(TableCell);
-const GET_MyDevice = gql`query { me{mydevices{type_device, brand_device, module_device, notes,  verification_device, calibration next_calibration} }}`;
-function MyDevice({t,className, rest}) {
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '90%',
+        overflowX: 'auto',
+        marginRight:"auto",
+        marginLeft:"auto",
+        height:"100%"
+    },
+    table: {
+maxWidth:"100%",
+        maxHeight:"500px",
+
+        color:"#fff"
+    },
+    inner:{
+        minWidth: "100%"
+    },
+    row:{
+        backgroundColor:"#fff"
+    }
+}));
+
+
+const GET_MyDevice = gql`query { me{mydevices{type_device, brand_device, module_device, verification_device calibration next_calibration} }}`;
+
+
+function MyDevice({t}){
     const classes = useStyles();
+    const [completed, setCompleted] = React.useState(0);
+    React.useEffect(() => {
+        function progress() {
+            setCompleted(oldCompleted => {
+                if (oldCompleted === 100) {
+                    return 0;}
+                const diff = Math.random() * 10;
+                return Math.min(oldCompleted + diff, 100);
+            });}
+        const timer = setInterval(progress, 500);
+        return () => {
+            clearInterval(timer);
+        };
+    }, [1]);
+    const handleBack = () => {
+    history.goBack();
+};
+    return (
+        <div>
+            <UserMenu/>
+            <main style={{ flexGrow: 1, height: '100%', overflow: 'auto'}}>
 
-    const [expanded, setExpanded] = React.useState(false);
-    const [component, setComponent] = React.useState(' ');
-    const handleChange = panel => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-    };
- const refreshPage=()=>{ 
-    window.location.reload(false); 
-}
-    return(
-       <div>
-
-                <UserMenu/>
-                <main style={{ flexGrow: 1, height: '100%', overflow: 'auto'}}>
-                    <div  className={classes.root}>
-                        <Grid container spacing={1}>
-                            <Grid item >
-                    <AppBar position="static" elevation={0} style={{borderBottom: '1px solid rgba(0, 0, 0, 0.12)',backgroundColor:"transparent", marginTop:"10%"}} >
-                        <Toolbar>
-                            <Grid container spacing={2} alignItems="center">
-                                <Grid item xs>
-                                    <Typography variant="h5" component="h2">{t('My devices')}</Typography>
-                                </Grid>
-
-                                <Grid item>
-                                    <span className={classes.spacer} />
-                                    <Button color="primary" variant="contained" >
-                                        <Link component={RouterLink} to="/add-device" variant="h8" style={{color:"#fff"}} >{t('Add Device')}</Link>
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </Toolbar>
-                    </AppBar>
-                            </Grid>
-                            <Grid item xs={12}>
-                        <Query query={GET_MyDevice} fetchPolicy={"network-only"} pollInterval={500} >
+        <div  className={classes.root}>
+            <Grid container spacing={1}>
+                <Grid item >
+                    <IconButton onClick={handleBack}>
+                        <ArrowBackIcon style={{color:"white", marginTop:"5%", marginRight:"auto"}} />
+                    </IconButton>
+                </Grid>
+                <Grid item xs={12}>
+             <Query query={GET_MyDevice} fetchPolicy={"network-only"} pollInterval={500} >
                             {( {loading, error, data} ) =>  {
-                                if (loading) {return <LinearDeterminate/>}
+                                if (loading) {return <span>loading</span>}
                                 if (error) { return error.message }
                                 const devicelist = data.me.mydevices;
-                                if(authToken){
-                                    return(
-                                        <Paper className={classes.table}>
-                                            <Table stickyHeader aria-label="sticky table">
-                                                <TableHead  >
-                                                    <TableRow >
-                                                        <StyledTableCell align="right">{t('Device')}</StyledTableCell>
+
+                   if(authToken){
+                        return(
+                            <Paper className={classes.table} >
+                                    <Table stickyHeader aria-label="sticky table">
+                                        <TableHead >
+                                            <TableRow >
+                                                 <StyledTableCell align="right">{t('Device')}</StyledTableCell>
                                                         <StyledTableCell align="right">{t('Category')}</StyledTableCell>
                                                         <StyledTableCell align="right">{t('Module')}</StyledTableCell>
-
                                                         <StyledTableCell align="right">{t('Calibration')}</StyledTableCell>
                                                         <StyledTableCell align="right">{t('Next Calibration')}</StyledTableCell>
                                                         <StyledTableCell align="right">{t('Delete')} </StyledTableCell>
-                                                    </TableRow>
-                                                </TableHead>
-                                                <TableBody>
-
-                                                    {devicelist.map(device =>(
-                                                        <TableRow>
-                                                            <TableCell lign="center">{device.brand_device}</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {devicelist.map(device =>(
+                                                <TableRow>
+                                                     <TableCell lign="center">{device.brand_device}</TableCell>
                                                             <TableCell lign="center">{device.type_device}</TableCell>
                                                             <TableCell lign="center">{device.module_device}</TableCell>
-
                                                             <TableCell lign="center">{device.calibration}</TableCell>
                                                             <TableCell lign="center">{device.next_calibration}</TableCell>
                                                             <TableCell lign="center"><IconButton>
                                                                 <DeleteIcon className={classes.block}  />
                                                             </IconButton></TableCell>
-                                                        </TableRow>))}
-                                                </TableBody>
-                                            </Table>
-                                        </Paper>)}else return null}}
-                        </Query>
-                            </Grid>
-
-                        </Grid>
-                </div>
-
+                                                        </TableRow> ))}
+                                        </TableBody>
+                                    </Table>
+                               </Paper>)}
+                    else return null}
+                }
+            </Query>
+                </Grid>
+            </Grid>
+        </div>
             </main>
         </div>
-    )
 
-
+    );
 
 }
 export default withTranslation()(MyDevice)
