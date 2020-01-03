014f95acc337f1550c1ca7dac5d7ddb9236988b3
Nataliia Hachok <norbinatali@gmail.com>
2020-01-03 10:46:16 +0200

    Update SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -42,7 +42,7 @@ class SignUp extends Component{
                     <Grid item>
                     <div style={{marginTop:"0px"}} >
                         <IconButton onClick={this.handleBack}>
-                            <ArrowBackIcon style={{color:"white"}} />
+                            <ArrowBackIcon style={{}} />
                         </IconButton>
                     </div>
                     </Grid>
