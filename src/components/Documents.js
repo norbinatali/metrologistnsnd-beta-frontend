a94391061960d4a56b14e056c4f50ef5a1e2cdcb
Nataliia Hachok <norbinatali@gmail.com>
2020-01-03 11:01:42 +0200

    Update Documents.js

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -10,7 +10,7 @@ export default function Documents() {
             <div style={{ marginTop: 40, padding: 30 }}>
                 <Typography variant="h5" component="h2">History</Typography>
                 <div style={{ marginTop: 20, padding: 60 }}>
-                    <Grid container spacing={2} justify="center" style={{backgroundColor:"white"}}>
+                    <Grid container spacing={2} justify="center" >
                     </Grid>
                 </div>
 
