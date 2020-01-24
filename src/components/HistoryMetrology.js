c26d90615bb3dbc4e1239a04a80fb48e3054e2a0
Nataliia Hachok <norbinatali@gmail.com>
2020-01-24 20:59:50 +0200

    Update HistoryMetrology.js

--- src/components/HistoryMetrology.js
+++ src/components/HistoryMetrology.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import MenuUser from "../menu/MenuUser";
+
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import {Grid, makeStyles, Typography, withStyles} from "@material-ui/core";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import Card from "@material-ui/core/Card";
+
 import StepConnector from "@material-ui/core/StepConnector";
 import clsx from "clsx";
 import Check from '@material-ui/icons/Check';
@@ -19,13 +16,9 @@ import {withTranslation} from "react-i18next";
 import AssessmentTheory from "./AssessmentTheory";
 import UserMenu from "./UserMenu";
 import Paper from "@material-ui/core/Paper";
-import CardActions from "@material-ui/core/CardActions";
-import CardHeader from "@material-ui/core/CardHeader";
+
 import Divider from "@material-ui/core/Divider";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemText from "@material-ui/core/ListItemText";
+
 import IconButton from "@material-ui/core/IconButton";
 import {ArrowRightIcon} from "@material-ui/pickers/_shared/icons/ArrowRightIcon";
 import history from '../history'
