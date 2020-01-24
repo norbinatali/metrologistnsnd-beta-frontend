c41aafc5221453f6be17860db5980675ec718d3e
Nataliia Hachok <norbinatali@gmail.com>
2020-01-24 21:01:42 +0200

    Update AssessmentTheory.js

--- src/components/AssessmentTheory.js
+++ src/components/AssessmentTheory.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import {Grid, makeStyles, Typography} from "@material-ui/core";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import Card from "@material-ui/core/Card";
 import Paper from "@material-ui/core/Paper";
 import {withTranslation} from "react-i18next";
 
