eabef705ddbca6edbe07ffd682f1f307a4b58913
Nataliia Hachok <norbinatali@gmail.com>
2020-01-25 11:59:52 +0200

    Update MenuTabPanel.js

--- src/components/MenuTabPanel.js
+++ src/components/MenuTabPanel.js
@@ -1,7 +1,4 @@
-import React, { Component,useState, useEffect } from 'react';
-import SignUp from '../SignUp';
-import Input from "@material-ui/core/Input";
-
+import React, { Component,useState } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -9,28 +6,22 @@ import SwipeableViews from 'react-swipeable-views';
 import TextField from '@material-ui/core/TextField';
 import {withStyles, makeStyles, useTheme } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
-import {Grid, IconButton} from "@material-ui/core";
+import {Grid} from "@material-ui/core";
 import{Mutation} from 'react-apollo';
-import { AUTH_TOKEN , GC_USER_ID,CREATE_LETTER} from '../constants';
-import {PopupboxManager, PopupboxContainer} from 'react-popupbox';
+import { CREATE_LETTER} from '../constants';
 import '../style/login.css';
 import ContactUS from "./ContactUS";
-import Link from "@material-ui/core/Link";
 import { useSnackbar } from 'notistack';
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import ListItem from "@material-ui/core/ListItem";
-import {Button} from "@material-ui/core";
-import Icon from "@material-ui/core/Icon";
 import i18n from "../menu/translations/i18n";
 import gql from "graphql-tag";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Login from "./Login";
 import {withTranslation} from "react-i18next";
-import Paper from "@material-ui/core/Paper";
 import PropTypes from "prop-types";
 import FrontPic from "../menu/style/Screen Shot 2019-11-28 at 9.19.01 PM.png"
 import logo from "../menu/style/LogoMakr_6pZrzB.png"
