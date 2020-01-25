197d4b729f623aaebea6d427fbcdc6fe09a25f9d
Nataliia Hachok <norbinatali@gmail.com>
2020-01-25 11:47:07 +0200

    Update MyDevice.js

--- src/components/MyDevice.js
+++ src/components/MyDevice.js
@@ -1,26 +1,17 @@
 import React from 'react';
 
 import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
 import { withTranslation} from 'react-i18next';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import Paper from '@material-ui/core/Paper';
-import CardContent from "@material-ui/core/CardContent";
 import i18n from "../menu/translations/i18n";
 import UserMenu from "./UserMenu";
 import LinearDeterminate from "./LinearDeterminate";
-import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import history from '../history';
 import {AUTH_TOKEN, GC_USER_ID} from "../constants";
-import TableContainer from '@material-ui/core/TableContainer';
 import DeleteIcon from "@material-ui/icons/Delete"
 import MyDeviceForm from "./MyDeviceForm";
 
@@ -61,22 +52,7 @@ const useStyles = makeStyles(theme => ({
 function MyDevice({t}){
     const classes = useStyles();
     const [completed, setCompleted] = React.useState(0);
-    React.useEffect(() => {
-        function progress() {
-            setCompleted(oldCompleted => {
-                if (oldCompleted === 100) {
-                    return 0;}
-                const diff = Math.random() * 10;
-                return Math.min(oldCompleted + diff, 100);
-            });}
-        const timer = setInterval(progress, 500);
-        return () => {
-            clearInterval(timer);
-        };
-    }, [1]);
-    const handleBack = () => {
-    history.goBack();
-};
+   
     return (
         <div className={classes.container}>
             <UserMenu/>
