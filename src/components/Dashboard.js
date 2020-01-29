5cbb23851a11820356826544ebada799465cfbf5
Nataliia Hachok <norbinatali@gmail.com>
2020-01-29 13:50:02 +0200

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,65 +3,65 @@ import {withTranslation} from "react-i18next";
 import {makeStyles} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
-import {AUTH_TOKEN, CREATE_MY_DEVICE, GC_USER_ID} from '../constants';
-import gql from 'graphql-tag';
-import {Mutation, Query} from 'react-apollo';
 import ContactUS from "./ContactUS";
-import LinearDeterminate from "./LinearDeterminate";
 import UserMenu from "./UserMenu";
+import gql from "graphql-tag";
+import {Mutation, Query} from 'react-apollo';
+import LinearDeterminate from "./LinearDeterminate";
+import {AUTH_TOKEN} from "../constants";
+const authToken = localStorage.getItem(AUTH_TOKEN);
 const useStyles = makeStyles(theme => ({
     root: {
         width: '100%',
+        marginLeft: "5%",
+        marginRight:"30%"
     },
 
 }));
-const authToken = localStorage.getItem(AUTH_TOKEN);
- const QUERY_USER = gql`query{me {name, email,country, companyName}}`;
- function Dashboard ({t}){
+const QUERY_USER = gql`query{me {name, email,country, companyName}}`;
+function Dashboard ({t}){
 
-     const classes = useStyles();
-        return(
-<div>
+    const classes = useStyles();
+    return(
+        <div>
             <UserMenu/>
-                <main style={{ flexGrow: 1, height: '100%', overflow: 'auto'}}>
-             <div style={{ marginTop: 40 }}>
-                        <div style={{ marginTop: 20, padding:30}}>
-            <div className={classes.root}>
-             <Query query={QUERY_USER}>
-                    {({loading, error, data}) => {
-                        if (loading) {
-                            return <LinearDeterminate/>
-                        }
-                        if (error) {
-                            return error.message
-                        }
-                        const userInfo = data.me;
-                        console.log(data.me.name);
-                        console.log(data.me.email);
-                        if (authToken&& data) {
-                            return (
-            
-<div >
-            <Typography  align={"justify"} style={{color:"rgba(0,1,47,0.84)"}} >{t('Welcome')}</Typography><Typography> {data.me.name}</Typography><br/>
-<Typography  align={"justify"} style={{color:"rgba(0,1,47,0.84)"}} >
-    <p>{t('About Metrologist')}<br/></p>
-    <p>{t('Why Metrologist is needed')}<br/></p>
-    <p>{t('Metrologist test')} <br/></p>
-    <p>{t('Metrologist recommendations')}<br/></p>
-</Typography>
+            <main style={{ flexGrow: 1, height: '100%', overflow: 'auto'}}>
+                <div style={{ marginTop: 40 }}>
+                    <div style={{ marginTop: 20, padding:30}}>
+                        <Query query={QUERY_USER}>
+                            {({loading, error, data}) => {
+                                if (loading) {
+                                    return <LinearDeterminate/>
+                                }
+                                if (error) {
+                                    return error.message
+                                }
+                                const userInfo = data.me;
+                                console.log(data.me.name);
+                                console.log(data.me.email);
+                                if (authToken && data) {
+                                    return (
+        <div className={classes.root}>
 
-</div>
-            )}}}
-            </Query>
-                </div>
-         </div>
+            <div > <Typography  align={"justify"}  >{t('Welcome')} {data.me.name}</Typography>
+
+                <Typography  align={"justify"}  >
+                    <p>{t('Metrologist')}{t('About Metrologist')}<br/></p>
+                    <p>{t('Metrologist')}{t('Why Metrologist is needed')}<br/></p>
+                    <p>{t('Metrologist')}{t('Metrologist test')} <br/></p>
+                    <p>{t('Metrologist recommendations')}<br/></p>
+                </Typography>
+
+            </div>
+        </div>)}}}</Query>
                     </div>
-        </main>
- </div>
+                </div>
+            </main>
+        </div>
 
 
 
-        )
+    )
 
 
 
