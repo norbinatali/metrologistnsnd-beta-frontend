7b9d0f4793dad01a196c460b82d2187ec06587c1
Nataliia Hachok <norbinatali@gmail.com>
2020-02-02 09:07:03 +0200

    Update ForgetPassword.js

--- src/components/ForgetPassword.js
+++ src/components/ForgetPassword.js
@@ -3,7 +3,7 @@ import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
 import {Button} from "@material-ui/core";
 import {AUTH_TOKEN, GC_USER_ID, RESET_TOKEN} from '../constants';
-
+import LinearDeterminate from "./LinearDeterminate";
 import { makeStyles } from '@material-ui/core/styles';
 import history from "../history";
 import i18n from 'i18next';
@@ -12,31 +12,28 @@ import{Mutation} from 'react-apollo';
 import Paper from "@material-ui/core/Paper";
 import Snackbar from "@material-ui/core/Snackbar";
 import { SnackbarProvider, useSnackbar } from 'notistack';
+const FORGET_PASSWORD = gql `mutation ($email:String!) { triggerPasswordReset(email: $email){ ok,resetToken }}`;
+       
+function ForgetPassword ({t}) {
+ const { enqueueSnackbar } = useSnackbar();
+  const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const confirm = async (data) => {
+       saveUserData(data.triggerPasswordReset.resetToken);
+      enqueueSnackbar(i18n.t('Check your email')
+       
+           };
 
-export default class ForgetPassword extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            forgetpassword:true,
-            id: "",
-            email: '',
-            password: '',
-            classes: true,
-            
-        };
+    const saveUserData =( resetToken) => {
+        localStorage.setItem(RESET_TOKEN, resetToken);
     }
-
-    render() {
-        const FORGET_PASSWORD = gql `mutation ($email:String!) { triggerPasswordReset(email: $email){ ok,resetToken }}`;
-        const { enqueueSnackbar } = this.props;
-        const { email} = this.state;
+        
         return(
             <div>
                 <Paper>
                 <label  htmlFor="email">Email </label>
-                <TextField type="text" value={this.state.email} onChange={e => {this.setState({ email: e.target.value })}}/>
-                <Mutation mutation={FORGET_PASSWORD}  variables={{ email } } onError={(error) => this.props.enqueueSnackbar(error.message)} onCompleted={(data) => this._confirm(data)}>
+                <TextField type="text" value={email} onChange={e => setState( e.target.value )}/>
+                <Mutation mutation={FORGET_PASSWORD}  variables={{ email } } onError={(error) => enqueueSnackbar(error.message)} onCompleted={(data) => this._confirm(data)}>
                     {( mutation,{loading, error}) => {
                 if (loading) { return (<LinearDeterminate /> )}
 
@@ -46,14 +43,7 @@ export default class ForgetPassword extends Component {
                     </Paper>
             </div>
         )
-    }
-    _confirm = async (data) => {
-        this._saveUserData(data.triggerPasswordReset.resetToken);
-        this.props.enqueueSnackbar(i18n.t('Check your email')
-       
-           };
-
-    _saveUserData =( resetToken) => {
-        localStorage.setItem(RESET_TOKEN, resetToken);
-    }
+    
+   
 }
+export default withTranslation
