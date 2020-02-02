f7142849c6ac78613b9e3260d5d67ee85d316db5
Nataliia Hachok <norbinatali@gmail.com>
2020-02-02 08:56:07 +0200

    Update ForgetPassword.js

--- src/components/ForgetPassword.js
+++ src/components/ForgetPassword.js
@@ -5,11 +5,13 @@ import {Button} from "@material-ui/core";
 import {AUTH_TOKEN, GC_USER_ID, RESET_TOKEN} from '../constants';
 
 import { makeStyles } from '@material-ui/core/styles';
-
+import history from "../history";
+import i18n from 'i18next';
 import gql from 'graphql-tag';
 import{Mutation} from 'react-apollo';
 import Paper from "@material-ui/core/Paper";
-
+import Snackbar from "@material-ui/core/Snackbar";
+import { SnackbarProvider, useSnackbar } from 'notistack';
 
 export default class ForgetPassword extends Component {
 
@@ -21,18 +23,20 @@ export default class ForgetPassword extends Component {
             email: '',
             password: '',
             classes: true,
+            
         };
     }
 
     render() {
         const FORGET_PASSWORD = gql `mutation ($email:String!) { triggerPasswordReset(email: $email){ ok,resetToken }}`;
+        const { enqueueSnackbar } = this.props;
         const { email} = this.state;
         return(
             <div>
                 <Paper>
                 <label  htmlFor="email">Email </label>
                 <TextField type="text" value={this.state.email} onChange={e => {this.setState({ email: e.target.value })}}/>
-                <Mutation mutation={FORGET_PASSWORD}  variables={{ email } } onError={(error) => enqueueSnackbar(error.message)} onCompleted={(data) => this._confirm(data)}>
+                <Mutation mutation={FORGET_PASSWORD}  variables={{ email } } onError={(error) => this.props.enqueueSnackbar(error.message)} onCompleted={(data) => this._confirm(data)}>
                     {( mutation,{loading, error}) => {
                 if (loading) { return (<LinearDeterminate /> )}
 
@@ -45,6 +49,8 @@ export default class ForgetPassword extends Component {
     }
     _confirm = async (data) => {
         this._saveUserData(data.triggerPasswordReset.resetToken);
+        this.props.enqueueSnackbar(i18n.t('Check your email')
+       
            };
 
     _saveUserData =( resetToken) => {
