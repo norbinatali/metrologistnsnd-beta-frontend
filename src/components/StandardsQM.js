b37b5c8c2dcc398d8b0e7a836805a9f478219c5d
Nataliia Hachok <norbinatali@gmail.com>
2020-02-21 11:06:41 +0200

    Update StandardsQM.js

--- src/components/StandardsQM.js
+++ src/components/StandardsQM.js
@@ -123,7 +123,7 @@ function StandardsQM({t}){
                     if (i18n.language === "en"){
                         return(
                              <Paper className={classes.table}>
-                            <MaterialTable  title={i18n.t('IR')}
+                            <MaterialTable  title={i18n.t('QM')}
                                             columns={[{title: i18n.t('Device'), field: 'device'},{title:i18n.t('Reglament'), field:'category'},{title:i18n.t('Module'), field:'module'}]}
                                                        data={devicelist.map((dev)=> ({device:dev.name_EN, category:dev.tr.name_TR_EN, module:dev.module}))}
                                                        options={{
