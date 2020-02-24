a4b30697ee95b73d370fd45adf7ee1c6557d6e8b
Nataliia Hachok <norbinatali@gmail.com>
2020-02-24 10:44:23 +0200

    Update StandardsM.js

--- src/components/StandardsM.js
+++ src/components/StandardsM.js
@@ -124,7 +124,7 @@ function StandardsM({t}){
                             <Paper className={classes.table}>
                             <MaterialTable  title={i18n.t('M')}
                                             columns={[{title: i18n.t('Device'), field: 'device'},{title:i18n.t('Reglament'), field:'category'},{title:i18n.t('Module'), field:'module'}]}
-                                                       data={devicelist.map((dev)=> ({device:dev.name_EN, category:dev.tr.name_TR_EN, module:dev.module}))}
+                                                       data={devicelist.map((dev)=> ({device:dev.name_EN, module:dev.module}))}
                                                        options={{
                                                            sorting: true,
                                                            rowStyle: {
