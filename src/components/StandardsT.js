dc511f0faa8e528f3a171677876b05b93d71967d
Nataliia Hachok <norbinatali@gmail.com>
2020-02-24 10:49:29 +0200

    Update StandardsT.js

--- src/components/StandardsT.js
+++ src/components/StandardsT.js
@@ -122,8 +122,8 @@ function StandardsT({t}){
                         return(
                            <Paper className={classes.table}>
                             <MaterialTable  title={i18n.t('T')}
-                                            columns={[{title: i18n.t('Device'), field: 'device'},{title:i18n.t('Reglament'), field:'category'},{title:i18n.t('Module'), field:'module'}]}
-                                                       data={devicelist.map((dev)=> ({device:dev.name_EN, category:dev.tr.name_TR_EN, module:dev.module}))}
+                                            columns={[{title: i18n.t('Device'), field: 'device'},{title:i18n.t('Module'), field:'module'}]}
+                                                       data={devicelist.map((dev)=> ({device:dev.name_EN, module:dev.module}))}
                                                        options={{
                                                            sorting: true,
                                                            rowStyle: {
