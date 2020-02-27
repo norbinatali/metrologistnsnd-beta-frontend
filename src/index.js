68984b853e4c8bb6967eb70eed024ab6d9f05c19
Nataliia Hachok <norbinatali@gmail.com>
2020-02-27 10:54:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import { onError } from "apollo-link-error";
 import { SnackbarProvider, useSnackbar } from 'notistack';
 const httpLink = createHttpLink({
     uri: 'https://metrologistnsnd-beta-backend.herokuapp.com/',
+    credentials: 'include'
 
 });
 
