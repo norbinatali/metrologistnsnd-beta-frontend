aa62970ba592326fe2a6d8e4704d217a44f396c6
Nataliia Hachok <norbinatali@gmail.com>
2020-03-03 10:09:50 +0200

    Update ConfirmResetPassword.js

--- src/components/ConfirmResetPassword.js
+++ src/components/ConfirmResetPassword.js
@@ -3,7 +3,7 @@ import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
 import {Button} from "@material-ui/core";
 import {AUTH_TOKEN, GC_USER_ID, RESET_TOKEN} from '../constants';
-
+import { withTranslation } from 'react-i18next';
 import { makeStyles } from '@material-ui/core/styles';
 
 import gql from 'graphql-tag';
@@ -11,7 +11,7 @@ import{Mutation} from 'react-apollo';
 import Paper from "@material-ui/core/Paper";
 
 
-export default class ConfirmResetPassword extends Component {
+ class ConfirmResetPassword extends Component {
 
     constructor(props) {
         super(props);
@@ -29,17 +29,17 @@ export default class ConfirmResetPassword extends Component {
     }
 
     render() {
-        const RESET_PASSWORD = gql `mutation ($email:String!, $resetToken:String!,$password:String! ) { passwordReset(email: $email, resetToken: $resetToken, password: $password){ id}}`;
+        const RESET_PASSWORD = gql `mutation ($email:String!, $resetToken:String!,$password:String! ) { passwordReset(email: $email, resetToken: $resetToken, password: $password){ id resetToken password}}`;
         const { email, resetToken, password} = this.state;
-
+       const { t } = this.props;
         return(
             <div>
                 <Paper >
-                    <label  htmlFor="password">Password </label>
+                    <label  htmlFor="password">{t('Password'} </label>
                     <TextField type="text" value={this.state.password} onChange={e => {this.setState({ password: e.target.value })}}/>
                     <Mutation mutation={RESET_PASSWORD}  variables={{email, resetToken, password } } onCompleted={() => this._confirm()}>
                         {mutation => (
-                            <Button style={{color:"rgba(0,1,47,0.84)"}} onClick={mutation}>Submit</Button>)}
+                            <Button style={{color:"rgba(0,1,47,0.84)"}} onClick={mutation}>{t('Submit')}</Button>)}
                     </Mutation>
 
                 </Paper>
@@ -47,16 +47,14 @@ export default class ConfirmResetPassword extends Component {
 
         )
     }
-    _confirm =() => {
-
-        const {resetToken} = this.props.confirmresetpassword;
-        this._saveUserData(resetToken);
+    _confirm =(resetToken) => {
+         this._saveUserData(resetToken);
 
     };
 
 
-    _saveUserData =(id, token, resetToken) => {
-        localStorage.setItem(GC_USER_ID, id);
-        localStorage.setItem(RESET_TOKEN, resetToken)
+    _saveUserData =(resetToken) => {
+              localStorage.setItem(RESET_TOKEN, resetToken)
     }
 }
+export default withTranslation() (ConfirmResetPassword)
