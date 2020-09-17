1c0b9e367b6f38c9a36ae468ff1c527fd960e756
Nataliia Hachok <norbinatali@gmail.com>
2020-09-17 14:28:21 +0300

    Update StandardsPR.js

--- src/components/StandardsPR.js
+++ src/components/StandardsPR.js
@@ -14,7 +14,7 @@ import Paper from '@material-ui/core/Paper';
 import CardContent from "@material-ui/core/CardContent";
 import i18n from "../menu/translations/i18n";
 import UserMenu from "./UserMenu";
-import LinearDeterminate from "./LinearDeterminate";
+import CircularProgressLoading from "./CircularProgressLoading";
 import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import history from '../history';
@@ -65,11 +65,7 @@ marginRight:"auto",
         backgroundColor:"#fff"
     }
 }));
-
-
 const GET_Device = gql`query { dtc(id:"ck4colwpvvg420922bssjy333") {id name_EN name_UA device_id{ name_UA name_EN module tr{name_TR_UA name_TR_EN} }}}`;
-
-
 function StandardsPR({t}){
     const classes = useStyles();
     const [completed, setCompleted] = React.useState(0);
@@ -90,10 +86,9 @@ function StandardsPR({t}){
                 <Grid item xs={12}>
             <Query query={GET_Device} >
                 {( {loading, error, data} ) =>  {
-                    if (loading) {return<LinearDeterminate/>}
+                    if (loading) {return<CircularProgressLoading/>}
                     if (error) { return <div>error</div>;}
                     const devicelist = data.dtc.device_id;
-
                     if (i18n.language === "ua" )  {
                         return(
                             <Paper className={classes.table} >
@@ -103,8 +98,7 @@ function StandardsPR({t}){
                                                        options={{
                                                            sorting: true,
                                                            rowStyle: {
-                                                               backgroundColor: '#EEE',
-                                                               
+                                                               backgroundColor: '#EEE',                
                                                            },
                                                        }}  localization={{
                                             body: {
@@ -113,12 +107,7 @@ function StandardsPR({t}){
                                             pagination: {labelRowsSelect:"рядків"}
                                                        }}
                                                         />
-
                                </Paper>)}
-
-
-
-
                     if (i18n.language === "en"){
                         return(
                               <Paper className={classes.table}>
